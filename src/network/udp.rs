136951bbc0266a6c2784c9656754c856d9f8862a
Daniel <golo7ov.danil@gmail.com>
2026-01-15 21:10:36 +0100

    First version of udp implementation

---
+++ src/network/udp.rs
@@ -0,0 +1,27 @@
+use tokio::net::UdpSocket;
+use std::net::SocketAddr;
+use std::io::Result;
+
+pub struct UdpServer {
+    socket: UdpSocket,
+}
+
+impl UdpServer {
+    pub async fn bind(addr: &str) -> Result<Self> {
+        let socket = UdpSocket::bind(addr).await?;
+        tracing::info!("UDP server listening on {}", addr);
+        Ok(Self { socket })
+    }
+
+    pub async fn recv(&self) -> Result<(Vec<u8>, SocketAddr)> {
+        let mut buf = vec![0u8; 1024];
+        let (len, addr) = self.socket.recv_from(&mut buf).await?;
+        buf.truncate(len);
+        Ok((buf, addr))
+    }
+
+    pub async fn send(&self, data: &[u8], addr: SocketAddr) -> Result<()> {
+        self.socket.send_to(data, addr).await?;
+        Ok(())
+    }
+}
