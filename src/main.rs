a1f9c3b0d192485b9a3518fe57c796902176ba4b
Daniel <golo7ov.danil@gmail.com>
2026-01-15 21:35:38 +0100

    Start server, wait for UDP packets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use rust_server::network::udp::UdpServer;
+use rust_server::protocol::client::ClientMessage;
+use prost::Message;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let server = UdpServer::bind("127.0.0.1:9000").await?;
+    tracing::info!("Game server started");
+
+    loop {
+        let (data, addr) = server.recv().await?;
+        tracing::debug!("Received {} bytes from {}", data.len(), addr);
+
+        match ClientMessage::decode(&data[..]) {
+            Ok(msg) => {
+                tracing::info!("Received message from {}: {:?}", addr, msg);
+            }
+            Err(e) => {
+                tracing::warn!("Failed to decode message: {}", e);
+            }
+        }
+    }
 }
