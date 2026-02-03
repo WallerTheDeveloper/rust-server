e4ca4fab170896179f01b31eb5179fa8a59e2938
Daniel <golo7ov.danil@gmail.com>
2026-02-03 21:10:39 +0100

    small debug log change

--- src/bin/paperio_server.rs
+++ src/bin/paperio_server.rs
@@ -130,7 +130,7 @@ async fn main() -> std::io::Result<()> {
         let (data, addr) = match server.recv().await {
             Ok(result) => result,
             Err(e) => {
-                tracing::debug!("recv error: {}", e);
+                tracing::debug!("Connection lost with client: {}", e);
                 continue;
             }
         };
